8fc9b09d73dd07203644cf32faafee8843c43a77
Anik Das <anikdas0811@gmail.com>
2024-12-02 21:46:15 +0530

    restart
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ActionPanel } from "./ui/components/action-panel";
-import { CanvasWorkArea } from "./ui/components/work-area";
-import { RootLayout } from "./ui/layouts/root";
-
-function App() {
-  return <RootLayout panel={<ActionPanel />} workArea={<CanvasWorkArea />} />;
-}
-
-export default App;
